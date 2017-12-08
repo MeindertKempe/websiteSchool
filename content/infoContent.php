dd407fefa8e7e6aabbdc6b0e516b374297356c43
Meindert Kempe <34239778+MeindertKempe@users.noreply.github.com>
2017-12-08 16:51:18 +0100

    Fix link to github being in English

--- content/infoContent.php
+++ content/infoContent.php
@@ -1,3 +1,3 @@
 <h1 class="h1">Info</h1>
 <p>Deze website gemaakt door Meindert Kempe.</p>
-<p>Source code can be found on <a href="https://github.com/MeindertKempe/websiteSchool">Github</a>.</p>
+<p>De source code is te zien op <a href="https://github.com/MeindertKempe/websiteSchool">Github</a>.</p>
