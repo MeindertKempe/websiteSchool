fd493d50125b86420058b30c3f8d2c3af2240a4b
Meindert Kempe <34239778+MeindertKempe@users.noreply.github.com>
2017-12-08 16:34:14 +0100

    Improve registration page

--- content/registerContent.php
+++ content/registerContent.php
@@ -1,9 +1,43 @@
+<?php
+include('scripts/registerScript.php')
+?>
+
 <h1 class="h1">Register</h1>
 <br>
-<form action="scripts/loginscript.php" method="post">
-	<p class="loginText">Username:</p><input class="loginField" type="text" name="username"><br>
-	<p class="loginText">Email:</p><input class="loginField" type="email" name="email"><br>
-	<p class="loginText">Password:</p><input class="loginField" type="password" name="password"><br>
-	<p class="loginText">Confirm password:</p><input class="loginField" type="password" name="confirmPassword"><br>
+
+<?=$invalidUsernameErr;?>
+<?=$invalidEmailErr;?>
+<?=$passwordLengthErr;?>
+<?=$passwordDiffErr;?>
+
+<form action="<?php filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_URL)?>" method="post">
+	<label class="loginText" for="username">Username:</label>
+	<input id="username" class="loginField" type="text" name="username" maxlength="127" minlength="4" 
+		value="<?php
+		if(!empty($_POST['username']))
+		{
+			echo($_POST['username']);
+		}
+		?>">
+	<br>
+	
+	<label class="loginText" for="email">Email:</label>
+	<input id="email" class="loginField" type="email" name="email" 
+		value="<?php
+		if(!empty($_POST['email']))
+		{
+			echo($_POST['email']);
+		}
+	   ?>">
+	<br>
+	
+	<label class="loginText" for="password">Password:</label>
+	<input id="password" class="loginField" type="password" name="password" 
+		   maxlength="255" minlength="4"><br>
+	
+	<label class="loginText" for="confirmPassword">Confirm password:</label>
+	<input id="confirmPassword" class="loginField" type="password" name="confirmPassword" 
+		   maxlength="255" minlength="4"><br>
+	
 	<input class="loginButton" type="submit" value="register">
 </form>
