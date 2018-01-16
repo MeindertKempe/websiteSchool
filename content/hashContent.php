da99372aaac2bbe9e53d2e9ea2864e0407e60fde
Meindert Kempe <34239778+MeindertKempe@users.noreply.github.com>
2018-01-16 13:58:26 +0100

    Add comments and rename variables in hashContent

--- content/hashContent.php
+++ content/hashContent.php
@@ -1,9 +1,10 @@
 <form class="hashForm" action="scripts/hashScript.php" method="post">
 	<textarea id="input" class="field" rows="10" cols="50" name="input" placeholder="Invoer"><?php
-	if(isset($_SESSION['input']))
+	// If there was any previous input add that
+	if(isset($_SESSION['hashInput']))
 	{
-		echo($_SESSION['input']);
-		unset($_SESSION['input']);
+		echo($_SESSION['hashInput']);
+		unset($_SESSION['hashInput']);
 	}
 	?></textarea>
 	<br>
@@ -11,13 +12,15 @@
 	<?php
 	//$algorithms = array('md2', 'md4', 'md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'ripemd128', 'ripemd160', 'ripemd256', 'ripemd320', 'whirlpool');
 	$algorithms = hash_algos();
+	// Add entry for every algorithm
 	foreach($algorithms as $var)
 	{
 		echo('<option value="' . $var .'"');
-		if(isset($_SESSION['algorithm']) && $var === $_SESSION['algorithm'])
+		// If a algorithm has previously been selected set that as default
+		if(isset($_SESSION['hashAlgorithm']) && $var === $_SESSION['hashAlgorithm'])
 		{
 			echo('selected');
-			unset($_SESSION['algorithm']);
+			unset($_SESSION['hashAlgorithm']);
 		}
 		echo('>' . $var . '</option>');
 	}
@@ -26,10 +29,10 @@
 	<input class="button" type="submit" value="submit">
 </form>
 <?php
-if(isset($_SESSION['output']))
+if(isset($_SESSION['hashOutput']))
 {
-	echo('<p>Hash: ' . $_SESSION['output'] . '<p>');
-	unset($_SESSION['output']);
+	echo('<p>Hash: ' . $_SESSION['hashOutput'] . '<p>');
+	unset($_SESSION['hashOutput']);
 }
 
 ?>
--- scripts/hashScript.php
+++ scripts/hashScript.php
@@ -1,16 +1,18 @@
 <?php
-// Assign input to variables
+
 if($_SERVER['REQUEST_METHOD'] == 'POST') 
 {
 	session_start();
+	// Assign input to variables
 	$input = filter_input(INPUT_POST, 'input', FILTER_SANITIZE_STRING);
 	$algorithm = filter_input(INPUT_POST, 'algorithm', FILTER_SANITIZE_STRING);
-	$_SESSION['input'] = $input;
-	$_SESSION['algorithm'] = $algorithm;
+	
+	$_SESSION['hashInput'] = $input;
+	$_SESSION['hashAlgorithm'] = $algorithm;
 	if(!empty($input) && !empty($algorithm))
 	{
 		$output = hash($algorithm, $input, false);
-		$_SESSION['output'] = $output;
+		$_SESSION['hashOutput'] = $output;
 	}
 	header('Location: ' . $_SESSION['currentPage']);
 }
