8df5512ceec9202328e9398225345f66d0b5853d
Meindert Kempe <34239778+MeindertKempe@users.noreply.github.com>
2018-01-19 10:26:25 +0100

    Move comment box to resources/comment.php

--- content/commentContent.php
+++ content/commentContent.php
@@ -1,16 +1,9 @@
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, consectetur, facere unde minima numquam ipsa rerum aliquam. Aperiam, illo, voluptatum deserunt sit vel laborum nisi laboriosam nam obcaecati nemo enim facere repellat voluptates cum nulla laudantium possimus rem praesentium debitis nesciunt at eos incidunt error placeat accusamus reiciendis molestias dolor beatae pariatur voluptatem atque ipsum corporis dolores. Adipisci, eos, in, corporis, id ipsum sed error recusandae magni quae voluptas perspiciatis quidem sequi eligendi illo nisi dolorum esse neque vitae culpa cupiditate maiores! Animi, dolores sequi fugiat minima odit sunt nobis ab porro suscipit reiciendis sapiente aut cum perspiciatis nisi inventore molestiae ex repellendus quae velit deserunt culpa quo voluptas reprehenderit pariatur mollitia incidunt possimus vitae natus aliquam enim ullam soluta magni accusamus tempora blanditiis iure iste exercitationem laborum at unde! Nemo reprehenderit eos culpa libero repudiandae quidem eius! Perspiciatis, corporis, quibusdam, ad rerum qui ea quam delectus nam quae excepturi neque quo inventore corrupti omnis saepe voluptatem a laboriosam soluta nostrum consectetur suscipit tenetur quasi. Blanditiis, nostrum, officia nihil architecto quasi ipsa rem odit dolorum iure voluptatum magnam suscipit. Ab, aliquid, quaerat eius eaque necessitatibus neque cumque harum ipsum consequatur incidunt sed saepe quae a praesentium reiciendis maxime recusandae iusto.</p>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, consectetur, facere unde minima numquam ipsa rerum aliquam. Aperiam, illo, voluptatum deserunt sit vel laborum nisi laboriosam nam obcaecati nemo enim facere repellat voluptates cum nulla laudantium possimus rem praesentium debitis nesciunt at eos incidunt error placeat accusamus reiciendis molestias dolor beatae pariatur voluptatem atque ipsum corporis dolores. Adipisci, eos, in, corporis, id ipsum sed error recusandae magni quae voluptas perspiciatis quidem sequi eligendi illo nisi dolorum esse neque vitae culpa cupiditate maiores! Animi, dolores sequi fugiat minima odit sunt nobis ab porro suscipit reiciendis sapiente aut cum perspiciatis nisi inventore molestiae ex repellendus quae velit deserunt culpa quo voluptas reprehenderit pariatur mollitia incidunt possimus vitae natus aliquam enim ullam soluta magni accusamus tempora blanditiis iure iste exercitationem laborum at unde! Nemo reprehenderit eos culpa libero repudiandae quidem eius! Perspiciatis, corporis, quibusdam, ad rerum qui ea quam delectus nam quae excepturi neque quo inventore corrupti omnis saepe voluptatem a laboriosam soluta nostrum consectetur suscipit tenetur quasi. Blanditiis, nostrum, officia nihil architecto quasi ipsa rem odit dolorum iure voluptatum magnam suscipit. Ab, aliquid, quaerat eius eaque necessitatibus neque cumque harum ipsum consequatur incidunt sed saepe quae a praesentium reiciendis maxime recusandae iusto.</p>
 
-<hr class="divider">
-<div class="commentWrapper">
-	<form class="commentForm" action="scripts/commentScript.php" method="post">
-		<input id="title" class="field commentTitleField" type="text" name="title" placeholder="Titel">
-		<br>
-		<textarea name="commentText" id="commentText" rows="10" class="field commentField" placeholder="Comment..."></textarea>
-		<br>
-		<input class="commentButton" type="submit" value="comment">
-	</form>
-</div>
+<?php
+include('resources/comment.php');
+?>
 <?php
 /*
 require_once('../config/commentDBName.php');
---
+++ resources/comment.php
@@ -0,0 +1,10 @@
+<hr class="divider">
+<div class="commentWrapper">
+	<form class="commentForm" action="scripts/commentScript.php" method="post">
+		<input id="title" class="field commentTitleField" type="text" name="title" placeholder="Titel">
+		<br>
+		<textarea name="commentText" id="commentText" rows="10" class="field commentField" placeholder="Comment..."></textarea>
+		<br>
+		<input class="commentButton" type="submit" value="comment">
+	</form>
+</div>
